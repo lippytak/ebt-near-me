07dd9b6e33abbc77912f7146fed74df4626cc450
jdungan <johnadungan@gmail.com>
2014-12-21 08:29:07 -0600

    add mustache cdn

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,16 @@ ebt = {
   user:{
     marker: null
   },
+  place:{
+    marker: null,
+    image : {
+      url: null,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    }
+  },
   infoWindow : new google.maps.InfoWindow(),
   directions_pre_link:"",
   options : {
@@ -23,24 +33,33 @@ ebt = {
 }
 
 ebt.markers = {
-  types : [
+    types: [
     {
-      style:   {where: "type IN ('ATM', 'POS') AND surcharge = '0'", markerOptions: {iconName: 'grn_circle'}},
+      style:  {
+        where: "type IN ('ATM', 'POS') AND surcharge = '0'",
+        iconName: 'grn_circle'
+      },
       legend: {
         title:'Free ATMs',
         color: 'green'
       }
     },
     {
-      style:     {where: "type IN ('ATM', 'POS') AND surcharge NOT EQUAL TO '0'", markerOptions: {iconName: 'ylw_circle'}},
+      style:  {
+        where: "type IN ('ATM', 'POS') AND surcharge NOT EQUAL TO '0'", 
+        iconName: 'ylw_circle'
+      },
       legend: {
         title:'Paid ATMs',
         color: 'yellow'
       }
     },
     {
-      style:    {where: "type = 'store'", markerOptions: {iconName: 'blu_circle'}},
-      legend:{
+      style:  {
+        where: "type = 'store'",
+        iconName: 'blu_circle'
+      },
+      legend: {
         title:'Stores that accept CalFresh',
         color: 'blue'
       }
@@ -56,6 +75,9 @@ ebt.markers = {
   },
   styles : function (i) {
     return ebt.markers.getArray('style',i)
+      .map(function (val) {
+        return {where: val.where, markerOptions: {iconName: val.iconName}}
+      })
   },
   legend : function (i) {
     return ebt.markers.getArray('legend',i)
@@ -96,12 +118,15 @@ ebt.googlemapOptions = {
 };
 
 ebt.utils = {
-  getPhrasesFromRow : function (row_hash) {
-    var phrases = {};
+  getPhrasesFromRow : function (row) {
+    phrases = {}
+    row_hash = {};
+    $.each( row, function( key, value ) {
+      row_hash[key] = value.value
+    });
     type = row_hash['type']
     text_address = row_hash['text_address']
-    directions_link = ebt.directions_pre_link + encodeURIComponent(text_address) + "' target='_blank'>" + text_address + "</a>"
-    phrases["directions_link"] = directions_link;
+    phrases["directions_link"] = ebt.directions_pre_link + encodeURIComponent(text_address) + "' target='_blank'>" + text_address + "</a>";
     location_name = row_hash['location_name']
     atm_name = row_hash['atm_name']
 
@@ -126,18 +151,22 @@ ebt.utils = {
         break;
     }
 
-    var cost_phrase;
-    surcharge = row_hash['surcharge']
-    cash_limit = row_hash['cash_limit']
-    if (surcharge == '0') {
-      cost_phrase = "It's <b>free</b> to use and you can get up to <b>$" + cash_limit + "</b>";
-    } else if (isNaN(surcharge)) {
-      cost_phrase = 'You can take out <b>$' + cash_limit + '</b> but you have to pay <b>2% fee</b>.';
-    } else {
-      cost_phrase = 'It costs <b>$' + surcharge + '</b> to use and you can get up to <b>$' + cash_limit + '</b>';
+    
+    phrases["cost_phrase"] = (function(surcharge,cash_limit){
+      switch (surcharge){
+        case '0':
+          cost_phrase = "It's <b>free</b> to use and you can get up to <b>$" + cash_limit + "</b>";
+          break;
+        case NaN:
+          cost_phrase = 'You can take out <b>$' + cash_limit + '</b> but you have to pay <b>2% fee</b>.';
+          break;
+        default:
+          cost_phrase = 'It costs <b>$' + surcharge + '</b> to use and you can get up to <b>$' + cash_limit + '</b>';  
+      }
+      
     }
-    phrases["cost_phrase"] = cost_phrase;
-
+    )(row_hash['surcharge'],row_hash['cash_limit'])
+    
     // See CfA Wufoo API docs for details
     wufoo_url = 'https://codeforamerica.wufoo.com/forms/ebtnearme-feedback/def/field3=' + type + '&field2=' + text_address;
     feedback_link_html = '<a href="' + wufoo_url + '">Report a problem with this location</a>'
@@ -290,6 +319,9 @@ $(document).ready(function () {
   });
   
   // add header
+  
+  // x=Mustache.render($("#must-header").html());
+  
   ebt.map.controls[google.maps.ControlPosition.TOP_LEFT ].push(document.getElementById('header'));
   ebt.searchBox = new google.maps.places.SearchBox(document.getElementById("address-input"));
 
@@ -311,19 +343,13 @@ $(document).ready(function () {
     place = ebt.searchBox.getPlaces()[0];
 
     // Get the icon, place name, and location.
-    var image = {
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(25, 25)
-    };
+    ebt.place.image = place.icon
 
     // Remove current marker if it exists and add the new one
-    if (ebt.user.marker) {ebt.user.marker.setMap(null)};
-    ebt.user.marker = new google.maps.Marker({
+    if (ebt.place.marker) {ebt.place.marker.setMap(null)};
+    ebt.place.marker = new google.maps.Marker({
       map: ebt.map,
-      icon: image,
+      icon: ebt.place.image,
       title: place.name,
       position: place.geometry.location
     });
@@ -332,21 +358,19 @@ $(document).ready(function () {
     ebt.map.setCenter(place.geometry.location)
     ebt.map.setZoom(ebt.options.default_zoom);
 
-    // Bias the SearchBox results towards places that are within the bounds of the
-    // current map's viewport.
-    google.maps.event.addListener(ebt.map, 'bounds_changed', function() {
-      var bounds = ebt.map.getBounds();
-      ebt.searchBox.setBounds(bounds);
-    });
   });
 
-  google.maps.event.addListener(ebt.fusion.data_layer, 'click', function(e) {
-    var row_hash = {};
-    $.each( e.row, function( key, value ) {
-      row_hash[key] = value.value
-    });
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
 
-    phrases = ebt.utils.getPhrasesFromRow(row_hash);
+  google.maps.event.addListener(ebt.map, 'bounds_changed', function() {
+    ebt.searchBox.setBounds(ebt.map.getBounds());
+  });
+
+
+  google.maps.event.addListener(ebt.fusion.data_layer, 'click', function(e) {
+
+    phrases = ebt.utils.getPhrasesFromRow(e.row);
 
     // Log click events in Google analytics
     if (type=='ATM'||type=='POS') {
